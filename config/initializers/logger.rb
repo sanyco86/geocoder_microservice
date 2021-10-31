cf0de0ef8ba9e7059749c685360049232e8864da
sanyco86 <lutsko86@gmail.com>
2021-10-31 13:04:38 +0200

    Ansible deploy

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,9 +1,6 @@
-logger = Ougai::Logger.new(
-  Application.root.concat('/', Settings.logger.path),
-  level: Settings.logger.level
-)
-
-logger.formatter = Ougai::Formatters::Readable.new if Application.environment == :development
+logg_dev = Application.environment.eql?('production') ? STDOUT : Application.root.concat('/', Settings.logger.path)
+logger = Ougai::Logger.new(logg_dev, level: Settings.logger.level)
+logger.formatter = Ougai::Formatters::Readable.new
 
 logger.before_log = lambda do |data|
   data[:service] = { name: Settings.app.name }
